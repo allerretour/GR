5abd4e21841a9eba1a8edcab71da42f92c0735c0
allerretour <aeuui.dvdps@gmail.com>
2025-06-18 12:39:01 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -857,7 +857,9 @@ function showTooltipModal(text, isHtml = false, shortcutName = "") {
     if (shortcutName) {
        const titleHtml = `<div style="margin-bottom: 10px; color: #555; text-align: center;">
   💡 Info du raccourci : <strong>${escapeHTML(shortcutName)}</strong>
-</div>`;
+</div>
+<hr style="border: none; border-top: 1px solid #ccc; margin: 5px 0;" />
+`;
         finalContent = titleHtml + finalContent;
     }
 
